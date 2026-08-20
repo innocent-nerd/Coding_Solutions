29d9763f6f16405062cf842b56859432d6e47266
innocent_nerd <debarshidas2020@gmail.com>
2026-08-20 08:58:44 +0530

    Day 4

---
+++ 1831A.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin>>n;
+        vector <int> vec(n);
+        for (int i=0;i<n;i++){
+            cin>>vec[i];
+            vec[i]=n-vec[i]+1;
+        }
+
+        for (int i=0;i<n;i++){
+            cout<<vec[i]<<" ";
+        }
+        cout<<"\n";
+
+
+
+
+       
+
+    }
+    return 0;
+}
