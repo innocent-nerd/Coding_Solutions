aa8d4d0bf1e2be8259fe203d64145440636859e1
innocent_nerd <debarshidas2020@gmail.com>
2026-08-22 01:52:45 +0530

    Day 5

---
+++ 1806A.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int x1,y1,x2,y2;
+        cin>>x1>>y1>>x2>>y2;
+
+        if (y2<y1){
+            cout<<"-1\n";
+            continue;
+        }
+
+        int dif=y2-y1;
+         x1=x1+dif;
+
+        if (x1<x2){
+            cout<<"-1\n";
+            continue;
+        }
+        
+        dif = dif + abs(x2-x1);
+        cout<<dif<<endl;
+        
+        
+        
+
+    }
+    return 0;
+}
